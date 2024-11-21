d213ffc09c74e09662abb7e7ede96b063b8c29a8
Sowmya Puttaswamygowda <cpsowmya9@gmil.com>
2024-11-21 12:11:39 +0100

    database connected

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,7 +12,7 @@ dotenv.config({ path: path.join(PATH, '..', '.env') });
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect(process.env.CONNECTION_URI);
+        await mongoose.connect(process.env.CONNECTION_URI).toString();
         console.log('Connected to the database successfully');
     } catch (err) {
         console.error(err);
